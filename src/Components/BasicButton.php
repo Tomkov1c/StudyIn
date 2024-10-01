9797c95a110b808d2551c349904068c3a4b83dbd
Tom Kliner <83597418+Tomkov1c@users.noreply.github.com>
2024-10-01 09:51:21 +0200

    css

--- src/Components/BasicButton.php
+++ src/Components/BasicButton.php
@@ -2,10 +2,10 @@
 
 function basicButton($text, $function = null, $fullWidth = false, $additionalClasses = "", $href = null) {
 
-    $classes = "h-[53px] justify-center items-center gap-[5px] inline-flex bg-[var(--color-primary)] 
-                px-[30px] py-[15px] outline outline-[var(--color-text)] outline-2] 
+    $classes = "justify-center items-center gap-[5px] inline-flex bg-[var(--color-primary)] 
+                px-[30px] py-[10px] outline outline-[var(--color-text)] outline-2] 
                 rounded-md shadow-[0px_4px_0px_3px_var(--color-text)] relative top-0 block
-                mx-0.5 font-sans font-medium";
+                mx-0.5 font-sans font-medium text-xl";
     $hover = "shadow-[0px_0px_0px_3px_var(--color-text)] top-1";
     $click = "bg-[var(--color-primary25)]";
     $animations = "transition-all duration-500 ease-[cubic-bezier(0,1,0,1)]";
--- src/index.php
+++ src/index.php
@@ -6,20 +6,44 @@
     <title>StudyIn</title>
     <?php include "Components/MustInclude.php"; ?>
 </head>
-<body class="light bg-[var(--color-background)] h-200 w-screen m-0 p-[75px]">
-    <section class="w-full justify-center items-center inline-flex h-[80%]">
+<body class="light bg-[var(--color-background)] w-screen m-0 p-[75px] flex flex-col gap-[100px]">
+    <section class="w-full justify-center items-center inline-flex h-[80vh]">
         <div class="w-1/2 self-stretch flex-col justify-center items-start inline-flex">
-            <h1 class="w-full text-[var(--color-text)] text-[64px] font-extrabold font-sans">Welcome To Your Academic Future</h1>
-            <h2 class="w-full h-[86px] text-[var(--color-text] text-xl font-semibold font-['Lexend Deca']">Loremalias unde at ullam amet nobis sed animi id aut dolore.</h2>
+            <h1 class="w-full text-[var(--color-text)] text-[64px] font-extrabold font-sans pt-[12rem]">Welcome To Your Academic Future</h1>
+            <h2 class="w-full h-[86px] text-[var(--color-text] text-xl font-semibold font-sans">Loremalias unde at ullam amet nobis sed animi id aut dolore.</h2>
             <div class="w-full px-0.5 justify-start items-end gap-[26px] inline-flex">
                 <?php echo basicButton("Sign Up")?>
                 <?php echo basicButton("Log In")?>
             </div>
-            <div class="left-0 top-[891.50px] justify-start items-start gap-[17px] inline-flex">
-                <p class="text-center text-[#0b0f13] text-[15px] font-normal font-sans"><i class="fa-solid fa-computer-mouse mr-2"></i>Scroll for more</p>
+            <div class="left-0 mt-auto mb-0 justify-start items-start gap-[17px] inline-flex">
+                <p class="text-center text-[#0b0f13] text-[15px] font-normal font-sans"><i class="fa-solid fa-angles-down mr-2"></i>Scroll for more</p>
             </div>
         </div>
-        <img class="w-1/2 grow shrink basis-0" src="images/notebook.png" />
+        <img class="w-1/2 my-auto grow shrink basis-0" src="images/notebook.png" />
+    </section>
+    <section class="w-full relative flex-col justify-center items-center gap-[26px] inline-flex">
+        <div class="self-stretch grow shrink basis-0 justify-start items-center gap-[26px] inline-flex">
+            <div class="grow shrink basis-0 self-stretch py-[145px] bg-[var(--color-primary)] rounded-[25px] border-2 border-[#0b0f13] flex-col justify-center items-center gap-2.5 inline-flex">
+                <div class="text-[var(--color-text)] text-[64px] font-bold font-sans">Negative Users</div>
+                <div class="text-[var(--color-text)] text-[15px] font-normal font-sans">Send help</div>
+            </div>
+            <div class="w-[328px] self-stretch py-[145px] bg-[var(--color-secondary)] rounded-[25px] border-2 border-[var(--color-text)] flex-col justify-center items-center gap-2.5 inline-flex">
+                <div class="text-[var(--color-text)] text-[50px] font-bold font-sans">100% Free</div>
+                <div class="text-[var(--color-text)] text-[15px] font-normal font-sans">Forever and ever</div>
+            </div>
+        </div>
+        <div class="self-stretch grow shrink basis-0 justify-start items-center gap-[26px] inline-flex">
+            <div class="grow shrink basis-0 self-stretch py-[145px] bg-[var(--color-text)] rounded-[25px] border-2 border-[var(--color-text)] justify-center items-center gap-[26px] flex">
+                <div class="h-[181px] flex-col justify-center items-center gap-2.5 inline-flex">
+                    <div class="text-[var(--color-background)] text-[64px] font-bold font-sans">Available on GitHub</div>
+                    <div class="self-stretch text-center text-[var(--color-background)] text-[15px] font-normal font-sans">With 1 stars and counting</div>
+                </div>
+            </div>
+            <div class="w-[509px] self-stretch py-[145px] bg-[var(--color-accent)] rounded-[25px] border-2 border-[var(--color-text)] flex-col justify-center items-center gap-2.5 inline-flex">
+                <div class="text-[var(--color-text)] text-[50px] font-bold font-sans">419M €</div>
+                <div class="text-[var(--color-text)] text-[15px] font-normal font-sans">in debt</div>
+            </div>
+        </div>
     </section>
 
 </body>
